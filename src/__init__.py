bd0e3aba2b2a1402b79d4c827ddfc20032e96526
K.H Lee <beta1360@naver.com>
2018-03-16 01:27:39 +0000

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,22 @@
+'''
+Since Mar 16, 2018
+Copyright by (c) Neogul-man :: kuj0210, KeonHeeLee, seok8418
+'''
+
+Organization = {
+  "Neogul-man" : "https://github.com/Neogul-man/Neogul-man"
+}
+
+Owner = {
+  "kuj0210" : "on_11@naver.com",
+  "KeonHeeLee" : "beta1360@naver.com",
+  "seok8418" : "seok8418@nate.com"
+}
+
+Maintainer =  {  
+  "kuj0210" : "on_11@naver.com",  
+  "KeonHeeLee" : "beta1360@naver.com",  
+  "seok8418" : "seok8418@nate.com"
+}
+
+LICENSE = None
